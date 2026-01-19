1143063adab73695b19b4390e84d7d3d4f32b64f
Shashank Raju Prathikantam <shashankraju@campx.in>
2026-01-19 18:00:45 +0530

    chore: add .env file for environment variables and update proxy target in rsbuild.config.ts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     port: 3012,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.REACT_API_HOST || 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: (path) => path.replace(/^\/api/, ''),
       },
